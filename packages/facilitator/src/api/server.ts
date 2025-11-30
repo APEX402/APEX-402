1e4c5b9974cdd251cb5f4db680f5a155d1d035a0
hwjustin <wanghuadi0145@gmail.com>
2025-11-30 19:59:49 +0000

    chore: add CORS Referrer

--- packages/facilitator/src/api/server.ts
+++ packages/facilitator/src/api/server.ts
@@ -25,7 +25,7 @@ export function createServer(
   app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Referrer");
 
     if (req.method === "OPTIONS") {
       res.sendStatus(200);
